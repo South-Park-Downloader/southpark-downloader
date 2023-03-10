da13191d5db1107cccdbb895f64c117a31adf191
Patrick <privat@skyraptor.eu>
2023-03-10 23:55:47 +0000

    Bind youtubedl and ffmpeg to the container,

--- src/ioc/container.ts
+++ src/ioc/container.ts
@@ -1,19 +1,28 @@
 import { Container as InversifyJS } from 'inversify';
+import FFMPEG, { FfmpegCommand } from 'fluent-ffmpeg';
 import App from '../app.js';
 import Commander from '../commander.js';
 import Database from '../south-park/database.js';
-import { TAppSymbol, TCommanderSymbol, TDatabaseSymbol } from './types.js';
+import { TAppSymbol, TCommanderSymbol, TDatabaseSymbol, TFFMPEGSymbol, TYouTubeDLSymbol } from './types.js';
+import { create } from 'youtube-dl-exec';
+import YouTubeDL from '../types/youtubedl.js';
 
 export class Container extends InversifyJS {
   async register(): Promise<void> {
+    /* Bind CommanderJS instance */
+    this.bind<Commander>(TCommanderSymbol).toConstantValue(new Commander());
+
     /* Bind App as singleton */
     this.bind<App>(TAppSymbol).to(App).inSingletonScope();
 
     /* Bind Database as singleton */
     this.bind<Database>(TDatabaseSymbol).to(Database).inSingletonScope();
 
-    /* Bind CommanderJS as singleton */
-    this.bind<Commander>(TCommanderSymbol).toConstantValue(new Commander());
+    /* Bind FFMPEG instance */
+    this.bind<FfmpegCommand>(TFFMPEGSymbol).toConstantValue(FFMPEG());
+
+    /* Bind YouTubeDL instance */
+    this.bind<YouTubeDL>(TYouTubeDLSymbol).toConstantValue(create('/usr/local/bin/youtube-dl'));
   }
 
   async boot(): Promise<void> {
@@ -22,6 +31,10 @@ export class Container extends InversifyJS {
 
     /* Load existing commands */
     await this.get<App>(TAppSymbol).loadCommands();
+
+    /* Configure FFMPEG */
+    this.get<FfmpegCommand>(TFFMPEGSymbol).setFfmpegPath('/usr/local/bin/ffmpeg');
+    this.get<FfmpegCommand>(TFFMPEGSymbol).setFfprobePath('/usr/local/bin/ffprobe');
   }
 }
 
