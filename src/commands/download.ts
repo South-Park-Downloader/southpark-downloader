89c46b4829d27ebe0c1091347fd09874c3a021bf
Patrick Henninger <privat@skyraptor.eu>
2023-03-12 22:26:19 +0000

    [Refactor] Properly ident

--- src/commands/download.ts
+++ src/commands/download.ts
@@ -12,7 +12,7 @@ export default class Download extends Command {
     filter: { 
       type: 'string',
       description: 'Filter episodes to be downloaded. Example: "S1E1", "S1E1-E10", "S1E1,S1E2-E10", "S2E3", "S1-S2", ...'
-     }
+    }
   };
 
   /**
