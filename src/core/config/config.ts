d341fb6298e68999bc0f23f35ff5cc431d12e516
Patrick <privat@skyraptor.eu>
2023-10-26 03:04:35 +0200

    save commit

---
+++ src/core/config/config.ts
@@ -0,0 +1,114 @@
+import configSchema, {ConfigSchema} from './config-schema.js';
+
+type ConfigInput = NestedTransform<ConfigSchema>;
+
+type ConfigDotNotations = DotNotations<ConfigSchema>;
+
+type ConfigKeyNotations = KeyNotations<ConfigSchema>;
+
+type ConfigGetReturnType<K extends DotNotation> = Exclude<
+  DotValue<ConfigSchema, K>,
+  undefined
+> | null;
+
+export default class Config {
+  private constructor(
+    /**
+     * Contains the parsed input data.
+     */
+    readonly parsed: ConfigSchema
+  ) {
+    //
+  }
+
+  /**
+   * Static factory method to create a new Config instance
+   * from the provided ConfigInput data.
+   */
+  static async parse(input: ConfigInput): Promise<Config> {
+    /* Initialize the Config object */
+    const instance = new Config(
+      /* Parse the provided input data */
+      await configSchema.parseAsync(input)
+    );
+
+    return instance;
+  }
+
+  test() {
+    const a = this.get('app.debug');
+    const b = a;
+
+    const c = this.access(this.parsed, ['app', 'debug']);
+    const d = c;
+  }
+
+  /**
+   * This method retrieves a value from the resolved configuration.
+   *
+   * @param key The key in dot.notation as defined in ConfigSchema / configSchema.
+   * @param fallback The value to return in case the configuration variable is undefined.
+   */
+  get<K extends ConfigDotNotations>(
+    key: K,
+    fallback: ConfigGetReturnType<K> = null
+  ): ConfigGetReturnType<K> {
+    /* Convert the provided "dot.notation" to keys */
+    const keys = this.splitDotNotation(key);
+
+    /* Access the value from the parsed config using the keys */
+    const result = this.access(this.parsed, keys);
+
+    /* Determine if there was a result */
+    if (result !== undefined) {
+      return result;
+    } else {
+      /* Return the fallback value to prevent undefined */
+      return fallback;
+    }
+  }
+
+  /**
+   * This method does format and return the provided "dot.notation" to key notation.
+   */
+  private splitDotNotation<K extends ConfigDotNotations>(
+    path: K
+  ): DotToKeyNotation<ConfigSchema, K> {
+    return path.split('.') as DotToKeyNotation<ConfigSchema, K>;
+  }
+
+  /**
+   * This method does access the provided object via the provided key notation.
+   * @param obj
+   * @param keys
+   * @returns
+   */
+  private access<O extends GenericRecord, K extends KeyNotations<O>>(
+    obj: O,
+    keys: K
+  ): KeyValue<O, K> | undefined {
+    /* Ensure that keys have been provided */
+    if (keys.length) {
+      /* Get and remove the first key */
+      const key = keys.shift();
+
+      /* Determine if the object contains the key */
+      if (typeof key === 'string' && key in obj) {
+        /* Get the value for the current key */
+        const value = obj[key];
+
+        /* Determine if there are remaining keys */
+        if (keys.length) {
+          /* There are more keys, access the next object */
+          return this.access(value, keys);
+        } else {
+          /* This is the last key, value is the result */
+          return value;
+        }
+      }
+    }
+
+    /* Return undefined in case nothing could be returned */
+    return undefined;
+  }
+}
--- src/types/utility.ts
+++ src/types/utility.ts
@@ -1,16 +1,4 @@
 /**
- * Utility type similar to Record except that it does provide generic string keys.
- */
-type GenericRecord<T = any> = {
-  [key: string]: T;
-};
-
-/**
- * Utility type to decide if T is an GenericRecord and provide a type for each case.
- */
-type IsGenericRecord<T, True, False> = T extends GenericRecord ? True : False;
-
-/**
  * Utility type to convert a string type to CONSTANT_CASE.
  * It does support:
  * - camelCase
@@ -26,9 +14,30 @@ type test = ConstantCase<'testCase'>;
 type a = test;
 
 /**
+ * Utility type similar to Record except that it does provide generic string keys.
+ */
+type GenericRecord<T = any> = {
+  [key: string]: T;
+};
+
+/**
+ * Utility type to decide if T is an GenericRecord and provide a type for each case.
+ */
+type IsGenericRecord<T, True, False> = T extends GenericRecord ? True : False;
+
+/**
+ * Utility type to transform the type of ALL properties of a nested object
+ * while respecting optional properties (? over undefined).
+ */
+type NestedTransform<O extends GenericRecord, T extends string = string> = {
+  [K in keyof O]: IsGenericRecord<O[K], NestedTransform<O[K], T>, T>;
+};
+
+/**
  * Utility type to represent a DotNotation for readablity.
  */
-type DotNotation<T extends string> = T;
+type DotNotation<T extends string = string> = T;
+
 /**
  * Utility type to get all possible "dot.notations" of a given type.
  */
@@ -56,28 +65,42 @@ type DotValue<O extends GenericRecord, K extends string> = K extends keyof O
   : never;
 
 /**
+ * Utility type to represent a key array for readablity.
+ */
+type KeyNotation<T extends Array<string> = Array<string>> = T;
+
+/**
+ * Utility type to determine if T is a KeyNotation.
+ */
+type IsKeyNotation<T> = T extends KeyNotation ? T : never;
+
+/**
+ * Utility type to retrieve all key notations of the provided object.
+ */
+type KeyNotations<O extends GenericRecord> = DotToKeyNotation<
+  O,
+  DotNotations<O>
+>;
+
+/**
+ * Utility type to retrieve the value type of a nested object property via key notation.
+ */
+type KeyValue<O, K extends KeyNotation> = K extends [infer First, ...infer Rest]
+  ? First extends keyof O
+    ? KeyValue<O[First], IsKeyNotation<Rest>>
+    : never
+  : O;
+
+/**
  * Utility type to retrieve the key array of the povided "dot.notation" path in the provided object.
  */
-type DotToKey<
+type DotToKeyNotation<
   O extends GenericRecord,
   K extends string
 > = K extends `${infer Key}.${infer Rest}`
   ? Key extends keyof O
-    ? [Key, ...DotToKey<O[Key], Rest>]
+    ? [Key, ...DotToKeyNotation<O[Key], Rest>]
     : never
   : K extends keyof O
   ? [K]
   : never;
-
-/**
- * Utility type to retrieve all keys arrays of the provided object.
- */
-type Keys<O extends GenericRecord> = DotToKey<O, DotNotations<O>>;
-
-/**
- * Utility type to transform the type of ALL properties of a nested object
- * while respecting optional properties (? over undefined).
- */
-type NestedTransform<O extends GenericRecord, T extends string = string> = {
-  [K in keyof O]: IsGenericRecord<O[K], NestedTransform<O[K], T>, T>;
-};
